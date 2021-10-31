da72043a380a08b947deba03634a7aa887f2f523
Illya Kysil <ikysil@ikysil.name>
2021-11-01 00:27:25 +0100

    feat: Implement file name filtering - including regex and lsbsysinit filters

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"regexp"
 	"sort"
+	"strings"
 
 	flag "github.com/spf13/pflag"
 )
@@ -51,6 +53,91 @@ func FindFiles(dir string) ([]os.FileInfo, error) {
 	return files, nil
 }
 
+func standardSuffixToIgnore() []string {
+	return []string{
+		"~",
+		",",
+		".disabled",
+		".cfsaved",
+		".rpmsave",
+		".rpmorig",
+		".rpmnew",
+		".swp",
+		",v",
+	}
+}
+
+func lsbSysInitSuffixToIgnore() []string {
+	return []string{".dpkg-old", ".dpkg-dist", ".dpkg-new", ".dpkg-tmp"}
+}
+
+func lsbSysInitRegexToAccept() []regexp.Regexp {
+	var r = []string{
+		`^[a-z0-9]+$`,                  // LANANA-assigned LSB hierarchical
+		`^_?([a-z0-9_.]+-)+[a-z0-9]+$`, // LANANA-assigned LSB reserved
+		`^[a-zA-Z0-9_-]+$`,             // Debian cron script namespaces
+	}
+	var result = []regexp.Regexp{}
+	for _, v := range r {
+		result = append(result, *regexp.MustCompile(v))
+	}
+	return result
+}
+
+func FilterFileName(fileName string) (bool, error) {
+	for _, s := range standardSuffixToIgnore() {
+		if strings.HasSuffix(fileName, s) {
+			return false, nil
+		}
+	}
+	if *lsbsysinit {
+		for _, s := range lsbSysInitSuffixToIgnore() {
+			if strings.HasSuffix(fileName, s) {
+				return false, nil
+			}
+		}
+		for _, r := range lsbSysInitRegexToAccept() {
+			if r.MatchString(fileName) {
+				return true, nil
+			}
+		}
+		return false, nil
+	}
+	if regex != nil {
+		var matched, err = regexp.MatchString(*regex, fileName)
+		return matched, err
+	}
+	return true, nil
+}
+
+func FilterFile(file os.FileInfo) (bool, error) {
+	if file.IsDir() {
+		return false, nil
+	}
+	return FilterFileName(file.Name())
+}
+
+func Run() error {
+	var files, err = FindFiles("../run-parts/test-report/")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var filesToProcess = []os.FileInfo{}
+	for _, file := range files {
+		var include, err = FilterFile(file)
+		if err != nil {
+			return err
+		}
+		if include {
+			filesToProcess = append(filesToProcess, file)
+		}
+	}
+	for _, file := range filesToProcess {
+		log.Printf("Found %s", file.Name())
+	}
+	return nil
+}
+
 func main() {
 	// Set properties of the predefined Logger, including
 	// the log entry prefix and a flag to disable printing
@@ -61,11 +148,5 @@ func main() {
 	if *test && *list {
 		log.Fatalln("--list and --test cannot be used together")
 	}
-	var files, err = FindFiles("../run-parts/test-report/")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	for _, file := range files {
-		log.Printf("Found %s", file.Name())
-	}
+	Run()
 }
